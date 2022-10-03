ce5abd72e9ec3c50002012d1537d2cf7ea34d820
deepanaik3 <101583309+deepanaik3@users.noreply.github.com>
2022-10-03 14:08:44 +0530

    Create project_operations.c

---
+++ 3_Implementation/src/project_operations.c
@@ -0,0 +1,82 @@
+/**
+ * @file project_operations.c
+ * @author Deepa R (dpanaik2000@gmail.com)
+ * @brief This the sourcecode which contains all functions for the project
+ * @version 0.1
+ * @date 2022-10-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "project_operations.h"
+/**
+ * @brief LIST OF BUS
+ * @return SHOW LIST OF BUS AVALIABLE
+ */
+void bus()
+{
+    return("SHOW LIST OF BUS AVALIABLE");
+}
+/**
+ * @brief NUMBER OF SEATS AVAILIABLE
+ * @return SHOW NUMBER OF SEATS AVAILIABLE
+ */
+void book()
+{
+    return("SHOW NUMBER OF SEATS AVAILIABLE");
+}
+/**
+ * @brief ENTER NAME
+ * @return DISPLAY NAME
+ */
+void name_n0(){
+    return("DISPLAY NAME");
+}
+/**
+ * @brief SELECTING A SEAT 
+ * @return CHOOSING SEATS
+ */
+int read_n0()
+{
+    return("CHOOSING SEATS");
+}
+/**
+ * @brief CONFIRMATION OF TICKET 
+ * @return SHOWS PASSENGER TICKET DEATILS
+ */
+void read_name()
+{
+    return("SHOWS PASSENGER TICKET DEATILS");
+}
+/**
+ * @brief LOCK THE SEAT 1
+ * @return CONFIRMATION OF SEATING ALLOTMENT 1
+ */
+void status()
+{
+    return("CONFIRMATION OF SEATING ALLOTMENT 1");
+}
+/**
+ * @brief LOCK THE SEAT 2
+ * @return CONFIRMATION OF SEATING ALLOTMENT 2
+ */
+void status_1()
+{
+    return("CONFIRMATION OF SEATING ALLOTMENT 2");
+}
+/**
+ * @brief   CANCELLING TICKET 
+ * @return TICKET CANCELLATION
+ */
+void cancel()
+{
+    return("TICKET CANCELLATION");
+}
+/**
+ * @brief USER LOGIN
+ * @return USER INFORMATION
+ */
+void login()
+{
+    return("USER INFORMATION");
+}
